f3c7580d79001f157a03cd73ba39e1a166d2b885
thalkirst <peter.bonyai@gmail.com>
2020-11-28 20:58:58 +0100

    works as requested and fixed bad practices

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
-const modal = document.getElementById("modalBox");
-const downloadButton = document.getElementById("button__download");
+const modal = document.querySelector(".modalBox");
+
+const downloadButton = document.querySelector(".button__download");
+downloadButton.addEventListener('click', popModal);
 
 function popModal() {
-    modal.className = "modal__popped";   
+    modal.className = "modal__popped";
+    setTimeout(() => {
+        const elements = document.querySelectorAll(".modal__closer, .button__closeModal");
+        elements.forEach(element => {
+            element.addEventListener('click', closeModal);
+        });
+    }, 300)
 }
 
 function closeModal() {
-    modal.className = "modal";
-}
-
-window.onclick = function(event) {
-    if (event.target !== modal) {
-        if (event.target !== downloadButton) {
-            modal.className = "modal";
-    }
-  }
+   modal.className = "modal";
+   const elements = document.querySelectorAll(".modal__closer, .button__closeModal");
+   elements.forEach(element => {
+       element.removeEventListener('click', closeModal);
+   });
 }
