c8537c5045cb80ac4ec44de3810dccdf991dd77e
Thalkirst <peter.bonyai@gmail.com>
2020-11-30 11:41:43 +0100

    fixed giving focus

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 const modal = document.querySelector(".modalBox");
+const firstFocusableElement = document.querySelector(".button__X");
 
 const downloadButton = document.querySelector(".button__download");
 downloadButton.addEventListener('click', popModal);
 
 function popModal() {
     modal.className = "modal__popped";
+    firstFocusableElement.focus();
     setTimeout(() => {
         const elements = document.querySelectorAll(".modal__closer, .button__closeModal");
         elements.forEach(element => {
